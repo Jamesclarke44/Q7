5ad7ad406b1651bf5cf2560d747e119bb353a639
Jamesclarke44 <132843061+Jamesclarke44@users.noreply.github.com>
2026-05-18 19:20:43 -0600

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,13 @@
+import json
+from config import FILE
 
+def load_data():
+    try:
+        with open(FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {"logs": [], "last_service": {}}
+
+def save_data(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
